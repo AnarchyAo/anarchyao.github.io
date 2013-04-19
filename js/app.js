cfa8d13649af0ae9e8283c13124b57c4688679a2
Serg 'Cust0dian' Nesterov <i.am.cust0dian@gmail.com>
2013-04-20 02:03:59 +0400

    Fix sidebars dissapear.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 $(".js-toggle").toggle(function() {
     $(this).stop().animate({
         width: "300px",
+        height: "100%"
     }, 500);
 }, function() {
     $(this).stop().animate({
         width: "50px",
+        height: "100%"
     }, 500);
 });
